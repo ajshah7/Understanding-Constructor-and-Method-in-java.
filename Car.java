40406babb55f7b2b0c917f04cdd34623da261703
Ajmal Shah <31572007+ajshah7@users.noreply.github.com>
2019-09-08 20:41:05 +0530

    Update Car.java

--- Car.java
+++ Car.java
@@ -4,8 +4,8 @@
 
 REMEMBER: CONSTRUCTOR -  has no return type and are as the same name as the class.
                          They can be called/involked without the .dot operator.
-          METHODS -  has a return type everytime evenvif there is no data to be returned, void 
-                     return type specifies that. always return the correct type.
+          METHODS -  has a return type everytime even if there is no data to be returned, void 
+                     return type specifies no data to be returned. always return the correct type.
           
 */
 package car;
@@ -16,18 +16,19 @@ public class Car  //class name car is the blueprint for the objects to work.
 
     private int maxspeed, noofdoors; // private type variable of type integer.
   
-    public Car(int speed,int doors) // a constructor,which are as the same name of a class and does not have a return type.
-    { // Its a parameterised constructor having two integer type variables for passing the value.
+    public Car(int speed,int doors) // a constructor,which has the same name of the class and does not have a return type.
+    { // Its a parameterised constructor having two integer type arguments for passing the value.
+       
         noofdoors = doors; //passing the values of given variable doors to noofdoors.
         
         maxspeed = speed;  // variable value passing as above.
         
-        System.out.println(" No of doors "+noofdoors+"\n Max speed "+maxspeed); // prints the value that is passed.
+        System.out.println(" No of doors " + noofdoors + "\n Max speed " + maxspeed); // prints the value that is passed.
     }
     
-    public int howfastcanitgo(int total) // a parameterised Method of return type int.
+    public int howfastcanitgo(int total) // a parameterised Method of return type int with one argument.
     {
-        int newspeed = maxspeed+total; //adiing new passed value to the maxspeed to get new speed.
+        int newspeed = maxspeed + total; //adding new passed value to the maxspeed to get new speed.
         
         return newspeed; // returning the value newspeed.
     }
